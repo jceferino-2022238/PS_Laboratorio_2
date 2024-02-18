2ea9209a39d807a07e5f32316b06f73965fe1162
jceferino-2022238 <jceferino-2022238@kinal.edu.gt>
2024-02-18 13:12:31 -0600

    Se actualizó el login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,80 +4,86 @@ const Profesor = require('../models/profesor');
 const bycryptjs = require('bcryptjs');
 const { generarJWTEstudiante } = require('../helpers/generar-jwt');
 const { generarJWTProfesor } = require('../helpers/generar-jwt') 
+const Role = require('../models/role');
 
-const loginEstudiante = async (req = request, res = response) =>{
-    const { correo, password } = req.body;
-
-    try{
-        const estudiante = await Estudiante.findOne({correo});
-
-        if(!estudiante){
-            return res.status(400).json({
-                msg: "Credenciales incorrectas, correo no existe en la base de datos."
+const login = async (req = request, res = response) =>{
+    const { correo, password, role } = req.body;
+    if (role == 'STUDENT_ROL') {
+        try{
+            const estudiante = await Estudiante.findOne({correo});
+            if(!estudiante){
+                return res.status(400).json({
+                    msg: "Credenciales incorrectas, correo no existe en la base de datos."
+                });
+            }
+            if(!estudiante.estado){
+                return res.status(400).json({
+                    msg: "El estudiante no existe en la base de datos."
+                })
+            }
+            const validarPassword = bycryptjs.compareSync(password, estudiante.password);
+            if(!validarPassword){
+                return res.status(400).json({
+                    msg: "La contraseña es incorrecta"
+                })
+            }
+            if(role == estudiante.role){
+                return res.status(400).json({
+                    msg: "El rol ingresado no coincide con el rol de las credenciales."
+                });
+            }
+            const token = await generarJWTEstudiante(estudiante.id);
+    
+            res.status(200).json({
+                msg: "Bienvenido al sistema, ingresó como estudiante",
+                estudiante,
+                token
+            })
+        } catch (e) {
+            console.log(e);
+            res.status(500).json({
+                msg: "Comuníquese con el admin"
             });
-        }
-        if(!estudiante.estado){
-            return res.status(400).json({
-                msg: "El estudiante no existe en la base de datos."
+        };
+    } else if(role == 'TEACHER_ROLE'){
+        try {
+            const profesor = await Profesor.findOne({correo});
+    
+            if(!profesor){
+                return res.status(400).json({
+                    msg: "Credenciales incorrectas, correo no existe en la base de datos."
+                })
+            }
+            if(!profesor.estado){
+                return res.status(400).json({
+                    msg: "El profesor no existe en la base de datos."
+                })
+            }
+            const validarPassword = bycryptjs.compareSync(password, profesor.password);
+            if (!validarPassword) {
+                return res.status(400).json({
+                    msg: "La contraseña es incorrecta"
+                })
+            }
+            const token = await generarJWTProfesor(profesor.id);
+            res.status(200).json({
+                msg: "Bienvenido al sistema, ingresó como profesor",
+                profesor,
+                token
             })
-        }
-        const validarPassword = bycryptjs.compareSync(password, estudiante.password);
-        if(!validarPassword){
-            return res.status(400).json({
-                msg: "La contraseña es incorrecta"
-            })
-        }
-        const token = await generarJWTEstudiante(estudiante.id);
-
-        res.status(200).json({
-            msg: "Bienvenido al sistema, ingresó como estudiante",
-            estudiante,
-            token
-        })
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
-            msg: "Comuníquese con el admin"
+        } catch (e) {
+            console.log(e);
+            res.status(500).json({
+                msg: "Comuníquese con el admin"
+            });
+        };
+    }else{
+        return res.status(400).json({
+            msg: "El rol enviado no coincide con el rol de las credenciales enviadas"
         });
-    };
-};
-
-const loginProfesor = async (req = request, res = response) =>{
-    const { correo, password} = req.body;
-    try {
-        const profesor = await Profesor.findOne({correo});
-
-        if(!profesor){
-            return res.status(400).json({
-                msg: "Credenciales incorrectas, correo no existe en la base de datos."
-            })
-        }
-        if(!profesor.estado){
-            return res.status(400).json({
-                msg: "El profesor no existe en la base de datos."
-            })
-        }
-        const validarPassword = bycryptjs.compareSync(password, profesor.password);
-        if (!validarPassword) {
-            return res.status(400).json({
-                msg: "La contraseña es incorrecta"
-            })
-        }
-        const token = await generarJWTProfesor(profesor.id);
-        res.status(200).json({
-            msg: "Bienvenido al sistema, ingresó como profesor",
-            profesor,
-            token
-        })
-    } catch (e) {
-        console.log(e);
-        res.status(500).json({
-            msg: "Comuníquese con el admin"
-        });
-    };
+    }
 }
 
 module.exports = {
-    loginEstudiante,
-    loginProfesor
+    login
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,26 +1,16 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { loginEstudiante } = require('../controllers/auth.controller');
-const { loginProfesor } = require('../controllers/auth.controller')
+const { login } = require('../controllers/auth.controller');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
 router.post(
-    '/loginEstudiante',
+    '/login',
     [
         check('correo', 'Este no es un correo válido').isEmail(),
         check('password', 'El password es obligatorio').not().isEmpty(),
         validarCampos
-    ], loginEstudiante);
-
-router.post(
-    '/loginProfesor',
-    [
-        check('correo', 'Este no es un correo válido').isEmail(),
-        check('password', 'El password es obligatorio').not().isEmpty(),
-        validarCampos
-    ], loginProfesor);
-
+    ], login);
 module.exports = router;
